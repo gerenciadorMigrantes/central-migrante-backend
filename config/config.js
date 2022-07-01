412e51f5c31303d951d2cd7c40a01873acb8de23
victor <victor.eduardo.sntos@gmail.com>
2022-06-30 23:20:44 -0300

    ajustes

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config({ path: './src/.env' });
+
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": "postgres"
+  }
+}
