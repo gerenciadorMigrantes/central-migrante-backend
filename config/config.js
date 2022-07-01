0350b140a78c3366ce7e7a1e78e6f0482d684b0a
victor <victor.eduardo.sntos@gmail.com>
2022-07-01 00:00:48 -0300

    ajustes

--- config/config.js
+++ config/config.js
@@ -15,14 +15,14 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
-  "production": {
+  production: {
     client: 'pc',
     dialect: 'postgres',
     connection: process.env.DATABASE_URL,
-    "username": process.env.NOME,
-    "password": process.env.SENHA,
-    "database": process.env.BANCO,
-    "host": '127.0.0.2',
-    "port": process.env.PORTA
+    username: process.env.NOME,
+    password: process.env.SENHA,
+    database: process.env.BANCO,
+    host: process.env.HOSTIP,
+    port: process.env.PORTA
   }
 }
