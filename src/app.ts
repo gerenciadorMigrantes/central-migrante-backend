01fac317ec4b5128fc6db93d798c328b234b2bb5
victor <victor.eduardo.sntos@gmail.com>
2022-07-02 19:38:48 -0300

    ajuste usuario login

--- src/app.ts
+++ src/app.ts
@@ -65,14 +65,7 @@ async function initial() {
 const app = express()
 const port = process.env.PORT;
 
-// app.use(cors("*"));
-app.use(function(req: Request, res : Response, next: NextFunction) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+app.use(cors());
 
 
 
