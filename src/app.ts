dad5ae112cb1012ff6cc1715d5190a094214e3ec
victor <victor.eduardo.sntos@gmail.com>
2022-07-02 17:37:18 -0300

    ajustes

--- src/app.ts
+++ src/app.ts
@@ -65,7 +65,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, "0.0.0.0", () => {
     console.log('Server running on port: ' + port)
 })
 
