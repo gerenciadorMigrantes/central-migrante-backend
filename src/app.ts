dcb1dbe2f2d8855fbb4dfd9b5c507d268ce8543e
victor <victor.eduardo.sntos@gmail.com>
2022-07-02 18:58:34 -0300

    ajustes app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-
 import indexRoute from './routers/index'
 import produtoRouter from './routers/produtoRoute'
 import usuarioRouter from './routers/usuarioRoute'
 import enderecoRouter from './routers/enderecoRoute'
-import { Sequelize } from "sequelize"
-const cors =  require("cors")
-const express =  require("express")
-require('dotenv').config({ path: __dirname+'/.env' });
+import {Sequelize} from "sequelize"
 
-// const Role = require("../models").Role
+const cors = require("cors")
+const express = require("express")
+require('dotenv').config({path: __dirname + '/.env'});
+
+const Role = require("../models").Role
 const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:pac123@localhost:5432/mydb', {
     logging: false,
     dialectOptions: {
@@ -19,10 +19,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres
     },
 })
 
-// sequelize.sync().then(() => {
-//     console.log('Resync Db');
-//     initial();
-//   });
+sequelize.sync().then(() => {
+    console.log('Resync Db');
+    initial();
+});
 
 export const sequelizeConnection = async () => {
     try {
@@ -38,29 +38,29 @@ export const sequelizeConnection = async () => {
 
 async function initial() {
 
-    // try{
-    //     await Role.create({
-    //         id: 1,
-    //         nome: "user",
-    //         descricao: "usuario comum"
-    //       });
-    //
-    //       await Role.create({
-    //         id: 2,
-    //         nome: "moderator",
-    //         descricao: "usuario moderador"
-    //       });
-    //
-    //       await Role.create({
-    //         id: 3,
-    //         nome: "admin",
-    //         descricao: "usuario administrador"
-    //       });
-    //
-    // }catch(e){
-    //     console.log("\n \n Não foi possivel criar papeis, pois já foram criados \n\n")
-    // }
-    
+    try {
+        await Role.create({
+            id: 1,
+            nome: "user",
+            descricao: "usuario comum"
+        });
+
+        await Role.create({
+            id: 2,
+            nome: "moderator",
+            descricao: "usuario moderador"
+        });
+
+        await Role.create({
+            id: 3,
+            nome: "admin",
+            descricao: "usuario administrador"
+        });
+
+    } catch (e) {
+        console.log("\n \n Não foi possivel criar papeis, pois já foram criados \n\n")
+    }
+
 }
 
 const app = express()
@@ -69,7 +69,7 @@ const port = process.env.PORT;
 app.use(cors());
 
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 app.listen(process.env.PORT, "0.0.0.0", () => {
