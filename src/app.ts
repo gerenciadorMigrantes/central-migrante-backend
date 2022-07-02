0e0d24b3c2c701d0fc3bbcfc64d41ef5b763210a
victor <victor.eduardo.sntos@gmail.com>
2022-07-02 17:41:28 -0300

    ajustes

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const cors =  require("cors")
 const express =  require("express")
 require('dotenv').config({ path: __dirname+'/.env' });
 
-const Role = require("../models").Role
+// const Role = require("../models").Role
 const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:pac123@localhost:5432/mydb', {
     dialectOptions: {
         ssl: {
@@ -17,10 +17,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres
     },
 })
 
-sequelize.sync().then(() => {
-    console.log('Resync Db');
-    initial();
-  });
+// sequelize.sync().then(() => {
+//     console.log('Resync Db');
+//     initial();
+//   });
 
 sequelize.authenticate().then(() => {
     console.log("db ok")
@@ -30,28 +30,28 @@ sequelize.authenticate().then(() => {
 
 async function initial() {
 
-    try{
-        await Role.create({
-            id: 1,
-            nome: "user",
-            descricao: "usuario comum"
-          });
-         
-          await Role.create({
-            id: 2,
-            nome: "moderator",
-            descricao: "usuario moderador"
-          });
-         
-          await Role.create({
-            id: 3,
-            nome: "admin",
-            descricao: "usuario administrador"
-          });
-
-    }catch(e){
-        console.log("\n \n Não foi possivel criar papeis, pois já foram criados \n\n")
-    }
+    // try{
+    //     await Role.create({
+    //         id: 1,
+    //         nome: "user",
+    //         descricao: "usuario comum"
+    //       });
+    //
+    //       await Role.create({
+    //         id: 2,
+    //         nome: "moderator",
+    //         descricao: "usuario moderador"
+    //       });
+    //
+    //       await Role.create({
+    //         id: 3,
+    //         nome: "admin",
+    //         descricao: "usuario administrador"
+    //       });
+    //
+    // }catch(e){
+    //     console.log("\n \n Não foi possivel criar papeis, pois já foram criados \n\n")
+    // }
     
 }
 
